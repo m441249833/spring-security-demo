43e06661d63d0bc6fbeb3ad2934cce09d274abfd
Mark <m5145692923@gmail.com>
2021-06-18 23:26:16 -0400

    add successfulauth role id

--- src/main/java/com/employee/demo/controller/UserController.java
+++ src/main/java/com/employee/demo/controller/UserController.java
@@ -7,6 +7,7 @@ import com.employee.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -60,6 +61,7 @@ public class UserController {
     }
 
     @DeleteMapping("/delete")
+    @PreAuthorize("hasRole('ADMINISTRATOR')")
     public ResponseEntity<?> deleteTask(@RequestParam("id") Long id){
         userService.deleteUser(id);
         return new ResponseEntity<>(HttpStatus.OK);
--- src/main/java/com/employee/demo/security/filter/TokenLoginFilter.java
+++ src/main/java/com/employee/demo/security/filter/TokenLoginFilter.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -82,6 +83,7 @@ public class TokenLoginFilter extends UsernamePasswordAuthenticationFilter {
 
         response.setStatus(HttpStatus.OK.value());
         JsonObject json = new JsonObject();
+        json.addProperty("ROLE",user.getUserInfo().getRole().getId());
         json.addProperty("TOKEN",token);
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         response.getWriter().write(json.toString());
